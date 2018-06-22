bc69be98319abe76a25bc772f4384796bd29cd8a
chunza2542 <chunza2542@hotmail.com>
2018-06-22 23:03:28 +0700

    inject global style

---
+++ src/core/helper.js
@@ -0,0 +1,54 @@
+import styled, {css} from 'styled-components'
+// style global variable and function belong here
+
+export const fonts = {
+  header: `Vollkorn, serif`,
+  normal: `Open Sans, sans-serif`,
+}
+
+export const fontSize = {
+  normal: 1.1,
+  subheader: 1.5,
+  header: 2,
+}
+
+export const colors = {
+  content: '#555',
+}
+
+// all helper function
+const sizes = {
+  screen: 1700,
+  desktop: 1200,
+  tablet: 600,
+  phone: 376,
+}
+
+// iterate through the sizes and create a media template
+export const media = Object.keys(sizes).reduce((accumulator, label) => {
+  const emSize = sizes[label]
+  accumulator[label] = (...args) => css`
+    @media (max-width: ${emSize}px) {
+      ${css(...args)}
+    }
+  `
+  return accumulator
+}, {})
+
+// all helper components
+export const Container = styled.div`
+  width: ${sizes.screen}px;
+  margin: auto auto;
+  ${media.screen`
+    width: auto;
+    margin: 0 30px;
+  `}
+  ${media.desktop`
+    width: auto;
+    margin: 0 20px;
+  `}
+  ${media.tablet`
+    width: auto;
+    margin: 0 10px;
+  `}
+`
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {injectGlobal} from 'styled-components'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './components/App'
+
+// global styled belong here
+injectGlobal`
+  body {
+    margin: 0;
+  }
+  * {
+    box-sizing: border-box;
+  }
+`
+
+ReactDOM.render(<App />, document.getElementById('root'))
